4d147b827c1f3cb6cd9927503e3c131a676162b0
mms <mark425e@stud.kea.dk>
2021-11-02 15:05:56 +0100

    Commit

--- src/Controller.java
+++ src/Controller.java
@@ -1,8 +1,33 @@
 public class Controller {
 
     public void run(){
+        UserInterface ui = new UserInterface();
+        Menu menu = new Menu();
+        while(true) {
+            int choice = ui.printMainMenu();
+            switch (choice){
+                case 1 -> {
+                    ui.printMenu(menu);
+                }
+                case 2 -> {
+                    ui.printBestillingsOversigt();
+                }
+                case 3 -> {
+                    opretOrdre();
 
+                }
+                case 4 -> {
+                    færdiggørBestilling();
+                }
+
+           }
+        }
+    }
+    public void opretOrdre(){
 
     }
 
+    public void færdiggørBestilling(){
+
+    }
 }
