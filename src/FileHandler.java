d29ce85d63b9e6160ee7feea6a47d1a80e19f3bf
Eliot996 <math10t6@stud.kea.dk>
2021-11-02 14:34:51 +0100

    Added getPizzasFromFile and final path strings

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -1,2 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class FileHandler {
+
+    private final String ARCHEIVED_ORDERS = "data/archived_orders.txt";
+    private final String MENU = "data/menu.txt";
+    private final String ACTIVE_ORDERS = "data/active_orders.txt";
+
+
+
+    public ArrayList<Pizza> getPizzasFromFile() {
+        ArrayList<Pizza> pizzas = new ArrayList<>();
+        try {
+            Scanner load = new Scanner(new File(MENU));
+            while(load.hasNextLine()){
+                pizzas.add(new Pizza(load.nextLine()));
+            }
+            return pizzas;
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
