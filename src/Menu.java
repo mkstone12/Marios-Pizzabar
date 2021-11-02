20aaf1fe178771778bd01c633df8c97709ad6e05
Eliot996 <math10t6@stud.kea.dk>
2021-11-02 14:56:59 +0100

    Added constructor and a getter to Menu

--- src/Menu.java
+++ src/Menu.java
@@ -8,13 +8,13 @@ public class Menu {
     public Menu(List<String> menuList){
         this.pizzasInMenu = new ArrayList<>();
         for(String item : menuList){
-            pizzasInMenu.add(new Pizza(item));
+            pizzasInMenu.add(new Pizza(item));// TODO: 02/11/2021 Add constructor that takes a csv string
         }
     }
 
     public Pizza getPizzaFromListNumber(int number){
         for (Pizza pizza : pizzasInMenu) {
-            if (pizza.getPizzaNr = number) return pizza;
+            if (pizza.getPizzaNr = number) return pizza; // TODO: 02/11/2021 add getPizzaNr to Pizza
         }
         return null;
     }
