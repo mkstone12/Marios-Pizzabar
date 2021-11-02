e47706837f3581a1ff1cbd7a4b7af0ffd0ae1375
Eliot996 <math10t6@stud.kea.dk>
2021-11-02 14:56:08 +0100

    Added constructor and a getter ot Menu

--- src/Menu.java
+++ src/Menu.java
@@ -1,2 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Menu {
+
+    private ArrayList<Pizza> pizzasInMenu;
+
+    public Menu(List<String> menuList){
+        this.pizzasInMenu = new ArrayList<>();
+        for(String item : menuList){
+            pizzasInMenu.add(new Pizza(item));
+        }
+    }
+
+    public Pizza getPizzaFromListNumber(int number){
+        for (Pizza pizza : pizzasInMenu) {
+            if (pizza.getPizzaNr = number) return pizza;
+        }
+        return null;
+    }
 }
