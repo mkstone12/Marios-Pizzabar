346db0f62d6e019671cb2f0516bbb7d19d21555c
mms <mark425e@stud.kea.dk>
2021-11-04 11:04:20 +0100

    Commit

--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -3,15 +3,10 @@ import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.UUID;
 
-/*public class Order {
-    private UUID id;
-    private ArrayList<OrderLine> orderLines;
-    private int ETA;
-}*/
+
 
 public class Order {
     private UUID orderID;
-    private static int count;
     private ArrayList<OrderLine> orderLines = new ArrayList<>();
     private double price;
     LocalTime orderDueTime = LocalTime.now();
