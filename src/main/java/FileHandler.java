4d51022204034fe807075e05429fb83ecdd2d116
Eliot996 <math10t6@stud.kea.dk>
2021-11-04 09:31:06 +0100

    Refactored storeActiveOrders

--- src/main/java/FileHandler.java
+++ src/main/java/FileHandler.java
@@ -33,8 +33,8 @@ public class FileHandler {
         return getStoredFromFile(ARCHIVED_ORDERS);
     }
 
-    public void storeActiveOrders(List<Order> orders)  {
-
+    public void storeActiveOrders(List<Order> orders) throws IOException {
+        saveToFile(convertOrdersToJson(orders), ARCHIVED_ORDERS);
     }
 
     private String convertOrdersToJson(List<Order> orders) throws JsonProcessingException {
