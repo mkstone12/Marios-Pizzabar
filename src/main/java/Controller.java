693bd39710fe8b7fc4a3ee8bae0a7d6bf83bf8cc
mms <mark425e@stud.kea.dk>
2021-11-04 10:21:29 +0100

    Commit

--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -37,14 +37,12 @@ public class Controller {
 
                 case 5 -> completeOrder();
 
-                case 0 -> keepGoing = saveAndQuite();
-
-
+                case 0 -> keepGoing = saveAndQuit();
             }
         }
     }
 
-    private boolean saveAndQuite() {
+    private boolean saveAndQuit() {
         try {
             fileHandler.storeActiveOrders(allActiveOrders);
             return false;
