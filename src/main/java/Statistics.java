b7b1a492161b36f4dd7bd3ea3b71249956021cf3
Eliot996 <math10t6@stud.kea.dk>
2021-11-05 15:04:47 +0100

    added invalid input handling to getRequestedDates() and added a few comments

--- src/main/java/Statistics.java
+++ src/main/java/Statistics.java
@@ -1,5 +1,6 @@
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.*;
 
 public class Statistics {
@@ -35,7 +36,7 @@ public class Statistics {
 
                 case 1 -> salesStats();
 
-                case 2 -> pizzaStats2();
+                case 2 -> pizzaStats();
 
                 case 3 -> getRequestedDates();
             }
@@ -66,8 +67,10 @@ public class Statistics {
     }
 
     public void pizzaStats2() {
+        // make new HashMap
         Map<String, Integer>  pizzaMap = new HashMap<>();
 
+        // populate Map with the string of a pizza, and the amount of times the pizza has been sold
         for (Order order : relevantOrders) {
             for (OrderLine line : order.getOrderLines()) {
                 String pizza = line.getPizza().toString();
@@ -79,17 +82,20 @@ public class Statistics {
             }
         }
 
+        // put the entries from the map into a list
         List<Map.Entry<String, Integer>> listOfPizzas = new LinkedList<>(pizzaMap.entrySet());
 
+        // sort the list, by the amount of times the pizza has been sold, in reverse order(largest to smallest)
         listOfPizzas.sort(((o1, o2) -> o2.getValue().compareTo(o1.getValue())));
 
-
+        // make the string of the sorted pizzas for print
         StringBuilder sb = new StringBuilder("Her er pizzaerne rangeret efter mest solget:\n");
         for (Map.Entry<String, Integer> entry : listOfPizzas){
             sb.append(entry.getValue()).append(" stk.: ").append(entry.getKey()).append('\n');
         }
 
-        ui.printTotalSales(sb.toString());
+        // print the sorted list of pizzas
+        ui.printTotalSales(sb.toString()); // TODO: 05/11/2021 Find better way to print
     }
 
     public void pizzaStats() {
@@ -138,15 +144,21 @@ public class Statistics {
     }
 
     public void getRequestedDates() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        startDate = LocalDate.parse(ui.getDate("fra"), formatter);
-        endDate = LocalDate.parse(ui.getDate("til"), formatter);
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+            startDate = LocalDate.parse(ui.getDate("fra"), formatter);
+            endDate = LocalDate.parse(ui.getDate("til"), formatter);
 
-        if (startDate.isAfter(endDate)){
-            ui.errorPrint("Warning: the start date you selected was after the end date.\n" +
-                    "Your start date has been set to one day before as your end date.");
-            startDate = endDate.minusDays(1);
+            if (startDate.isAfter(endDate)){
+                ui.errorPrint("Warning: the start date you selected was after the end date.\n" +
+                        "Your start date has been set to one day before as your end date.");
+                startDate = endDate.minusDays(1);
+            }
+            resetPizzaArrays();
+
+        } catch (DateTimeParseException e) { // if the user types an input the parser does not understand
+            ui.errorPrint("Warning: Invalid Input. Please check formating and try again:");
+            getRequestedDates();
         }
-        resetPizzaArrays();
     }
 }
